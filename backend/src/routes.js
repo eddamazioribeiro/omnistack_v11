178a2e0c62bc2f49e94c27a017aaf1c20c3a2249
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-03-29 04:58:04 -0300

    Created 'profile' endpoint, to list all ONGs' incidents.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,10 +2,14 @@ const express = require('express');
 const routes = express.Router();
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+
+// session
 
 // ongs
 routes.post('/ongs', OngController.create);
 routes.get('/ongs', OngController.index);
+routes.get('/profile', ProfileController.index);
 // incidents
 routes.post('/incidents', IncidentController.create);
 routes.get('/incidents', IncidentController.index);
