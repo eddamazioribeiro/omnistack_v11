f6191b2f8aeba5a58c322a93ab5c7ac6f6ca622b
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-03-29 05:53:24 -0300

    Installing cors package on backend app.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
+ape.use(cors());
 app.use(express.json());
 app.use(routes);
 
