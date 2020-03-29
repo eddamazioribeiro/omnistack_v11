e95a9d78168abbc4ca105c4a0e255cbb9c7174b5
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-03-29 04:15:46 -0300

    Created 'incidents' endpoints, with create (POST) and list (GET) for this resource.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const routes = express.Router();
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
-routes.get('/ongs', OngController.index);
+// ongs
 routes.post('/ongs', OngController.create);
+routes.get('/ongs', OngController.index);
+// incidents
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
 
 module.exports = routes;
